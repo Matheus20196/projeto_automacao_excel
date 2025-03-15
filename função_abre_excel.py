639eb1216274a8c62849f8779daaceecf7fcd0e2
Matheus Rodrigues <rodrigues_20191@outlook.com>
2025-03-15 20:25:24 -0300

    Criando função que abre o excel

---
+++ função_abre_excel.py
@@ -0,0 +1,19 @@
+import pyautogui as pg 
+import clipboard as cp
+import time
+
+def abre_excel():
+    time.sleep(1)
+    pg.press('Win')
+    time.sleep(1)
+    cp.copy('Excel')
+    time.sleep(1)
+    pg.write(cp.paste())
+    time.sleep(1)
+    pg.press('Enter')
+
+
+
+
+
+abre_excel()
